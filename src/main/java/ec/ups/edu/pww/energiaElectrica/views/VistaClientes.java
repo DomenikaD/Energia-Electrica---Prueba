5b8e7f3482aa31d04fee65d45332661e968ac629
Dome Delgado <ddelgadoi@est.ups.edu.ec>
2024-07-16 08:05:30 -0500

    Actualizacion de Clase XHTML VistaClientes

--- src/main/java/ec/ups/edu/pww/energiaElectrica/views/VistaClientes.java
+++ src/main/java/ec/ups/edu/pww/energiaElectrica/views/VistaClientes.java
@@ -2,19 +2,30 @@ package ec.ups.edu.pww.energiaElectrica.views;
 
 import java.util.List;
 
+import ec.ups.edu.pww.energiaElectrica.business.GestionCliente;
 import ec.ups.edu.pww.energiaElectrica.model.Cliente;
+import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
 import jakarta.inject.Named;
 
 @Named("clientes")
 @RequestScoped 
 public class VistaClientes {
 	
+	@Inject
+	private GestionCliente gClientes;
+	
 	private Cliente persona = new Cliente();
 	
 	
 	private List<Cliente> listado;
 	
+	
+	@PostConstruct
+	public void init() {
+		listado = gClientes.getClientes();  /*recupere en listado los clientes que tengo en mi BD*/
+	}
 
 	/*GETTERS & SETTERS*/
 	
@@ -35,6 +46,18 @@ public class VistaClientes {
 	}
 	
 	
+	public String consultar() {
+		System.out.println(this.persona);
+	
+	try {
+		gClientes.createCliente(persona);
+		return "listadoClientes?faces-redirect=true";
+		} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		return "error";
+		}
+	}
 
 
 }
